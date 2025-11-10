c62fa298e7e6dbf7f3ba95e46b3f67a3cc455e08
katarina-andrews <andrews.katarina@gmail.com>
2025-11-10 14:25:13 -0600

    installed dependencies, added cognito code and axios code

---
+++ src/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://ul4ag1di2b.execute-api.us-east-2.amazonaws.com/",
+  timeout: 10000,
+   headers: { "Content-Type": "text/plain;charset=UTF-8" },
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "react-oidc-context";
+import { WebStorageStateStore } from "oidc-client-ts";
 
-createRoot(document.getElementById('root')).render(
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_qND49wykO",
+  client_id: "4cd1t6k5jgtrn9v8ch0hgpnq4e",
+  redirect_uri: import.meta.env["VITE_LOCAL_URL"],
+  response_type: "code",
+  scope: "email openid phone",
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
+  // Crucial in an SPA without routes: remove ?code&state after processing
+  onSigninCallback() {
+    // strip query/hash so a refresh won't try to handle the callback again
+    window.history.replaceState({}, document.title, "/");
+  },
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </StrictMode>
+);
